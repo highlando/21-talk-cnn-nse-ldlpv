87f38a1cfff9d5601e4e6de8f1e93d54d96352c5
Jan Heiland <jnhlnd@gmail.com>
2021-07-07 21:50:18 +0200

    now the output is called index

--- prop-to-ghp.sh
+++ prop-to-ghp.sh
@@ -3,7 +3,7 @@ git add slides.md
 git commit -m 'updated the slides md'
 
 git checkout pandoc-outputs
-git merge main
+git merge master
 ./mkdc.sh
 git add index.html
 git commit -m 'updated the slides md and now the html'
